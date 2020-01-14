6b17099bff87fbda5a964f405ec619f94fb151b1
Nova Laurente <novalaurente@gmail.com>
2020-01-14 21:04:41 +0800

    Fix typo.

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 require('dotenv').config()
 
-const databaseUrl = process.env.DATABASE_URL || `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${proceses.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
+const databaseUrl = process.env.DATABASE_URL || `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
 mongoose.connect(databaseUrl, {
 	useNewUrlParser:true,
